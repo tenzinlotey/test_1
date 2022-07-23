513b2c6b3e3e99f07277dd91a8e297457a0f327d
Tenzin Lotey <teeonextenzin@gmail.com>
2022-07-23 08:19:09 +0530

    new pro

--- com.wipro.pro1/src/main/java/com/wipro/test/Application.java
+++ com.wipro.pro1/src/main/java/com/wipro/test/Application.java
@@ -4,15 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class Application {
-	
+
 	@GetMapping("/")
 	public String test() {
 		return "Hello world";
 	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
