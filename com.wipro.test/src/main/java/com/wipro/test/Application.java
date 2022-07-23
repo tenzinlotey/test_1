bbbb84e9d679b859baaddc249111a2b08562e0de
Tenzin Lotey <teeonextenzin@gmail.com>
2022-07-23 08:02:20 +0530

    added spring pro

---
+++ com.wipro.test/src/main/java/com/wipro/test/Application.java
@@ -0,0 +1,20 @@
+package com.wipro.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Application {
+	
+	@GetMapping("/")
+	public String test() {
+		return "Hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
